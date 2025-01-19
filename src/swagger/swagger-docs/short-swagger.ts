1fff9bc4ca7af7e36cc97fc47dc065e0de28fa84
PurushothamaGovindaraju <purushothama@octalogic.in>
2025-01-19 23:35:17 +0530

    swagger file directory updated

--- src/swagger/swagger-docs/short-swagger.ts
+++ src/swagger/swagger-docs/short-swagger.ts
@@ -21,7 +21,7 @@ const swaggerOptions: swaggerJsdoc.Options = {
     },
     security: [{ ApiKeyAuth: [] }],
   },
-  apis: ["src/swagger/swagger-model/*.ts"],
+  apis: ["src/swagger/swagger-model/*.ts", "dist/swagger/swagger-model/*.ts"],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
